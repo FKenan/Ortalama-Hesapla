d0f509c16bd2964676d5ea8fb15294f0893425db
FKenan <100311643+FKenan@users.noreply.github.com>
2022-07-02 22:27:52 +0300

    Add files via upload

---
+++ YakınSayıBul.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class YakınSayıBul {
+    public static void main(String[] args) {
+        String[][] a = new String[7][4];
+
+        for (int i=0;i< a.length ; i++)
+            for (int j = 0;j<a[i].length;j++){
+                if(i==0 || i== 3 || i==6)
+                    a[i][j] = " * ";
+                else if (j==0 || j==3)
+                    a[i][j] = " * ";
+                else
+                    a[i][j] = "   ";
+            }
+        for (String[] row:a){
+            for (String col:row)
+                System.out.print(col);
+            System.out.println();
+        }
+
+
+
+    }
+}
